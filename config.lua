056b9f2485f3fb2f53cf67bcae7fdeecd14d4a8c
GoldO <83804899+GoldO-dev@users.noreply.github.com>
2023-08-10 13:18:00 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,24 @@
+-- Add this to the qb-weapons/config.lua under line 3
+Config.HoldToReload = 2
+
+Config.forbiddenTypes = {
+    "AMMO_FLARE", 
+    "AMMO_BALL", 
+    "AMMO_MINIGUN", 
+    "AMMO_STINGER", 
+    "AMMO_GRENADELAUNCHER", 
+    "AMMO_RPG", 
+    "AMMO_SNIPER_REMOTE", 
+    "AMMO_STUNGUN", 
+    "nil"
+}
+
+Config.AmmoAmmount = {
+    ["pistol_ammo"] = 12,
+    ["smg_ammo"] = 20, 
+    ["shotgun_ammo"] = 10,
+    ["rifle_ammo"] = 30,
+    ["snp_ammo"] = 10,
+    ["mg_ammo"] = 30,
+    ["emp_ammo"] = 10
+}
